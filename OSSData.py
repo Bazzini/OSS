26318d08779e500fae3114834b428c59647d6aa7
Ashok Balakrishnan <balakrishnan6370@gmail.com>
2015-09-18 19:58:22 -0400

    Able to manipulate (search) list of dictionaries

--- OSSData.py
+++ OSSData.py
@@ -11,9 +11,22 @@ sheet = book.sheet_by_index(0)
 # read header values into the list
 keys = [sheet.cell(0, col_index).value for col_index in range(sheet.ncols)]
 
-dict_list = []
+student_list = []
 for row_index in range(1, sheet.nrows):
     d = {keys[col_index]: sheet.cell(row_index, col_index).value
          for col_index in range(sheet.ncols)}
-    dict_list.append(d)
+    student_list.append(d)
+
+count = 0
+studwithteach = []
+for student in student_list:
+    if student['StudentTeacherID'] == 'Rosemarie Klimasko':
+        studwithteach.append(student['FirstName'] + ' ' + student['LastName'])
+
+parentwithteach = []
+for student in student_list:
+    if student['StudentTeacherID'] == 'Rosemarie Klimasko':
+        parentwithteach.append(student['P1Email'])
+        if student['P2Email'] and student['P2Email'].strip():
+            parentwithteach.append(student['P2Email'])
 pass
